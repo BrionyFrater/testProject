22d77181449a4ddbf5c2a0f4a26dc52bbab900f6
BrionyFrater <briefrater@gmail.com>
2025-10-06 22:46:31 -0500

    added more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { useEffect } from 'react';
-
+import { Bug, RefreshCw, CircleX } from 'lucide-react';
 function App() {
   const [char, setChar] = useState({});
   const [count, setCount] = useState(0)
+  const [error, setError] = useState('')
   const myName = useRef();
   useGSAP(() => {
 
@@ -51,36 +50,56 @@ function App() {
     })
 
     gsap.to(myName.current, {
-      textShadow: "0 0 40px 10px #ffffffff",  // Center scale
+      textShadow: "0 0 40px #ffffff",  // Center scale
       ease: "power3.out",        // Correct GSAP easing
       duration: 0.8,
       delay: 2.8,
     })
   })
 
+  const getCharacter = async () => {
+    try {
+      throw new Error("broo");
+      if (error != "") setError("")
+    } catch (error) {
+      setError(`I'm sorry we couldn't get you character. ${error}`)
+    }
+
+    setChar({})
+  }
+
   useEffect(() => {
-    
+
   }, [])
 
   return (
-    <>
+    <div className='flex flex-col h-screen justify-center items-center'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h5 className='text-white/90 font-normal tracking-tighter'>Attack on Titan</h5>
       </div>
       <div className='yo will-change' ref={myName}>
-        <h1 className='will-change'>PixelForge</h1>
+        <h1 className='will-change'>Who Are You in AOT?</h1>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="card mt-9">
+        <button className='flex items-center justify-center gap-2 rounded-full
+  text-[#0f0f0f]
+  px-5 py-2.5
+  text-base font-semibold
+  bg-gradient-to-r from-emerald-600 to-lime-400
+  cursor-pointer
+  transition-colors duration-200
+   focus:outline-2 focus:outline-auto focus:outline-[#8a90fa]' onClick={getCharacter}>
+          <RefreshCw />
+          Get Character
         </button>
+        {error && (
+          <div className='mt-3 flex gap-2 items-center justify-center'>
+            <CircleX className='error' />
+            <p className='error'>{error}</p>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   )
 }
 
